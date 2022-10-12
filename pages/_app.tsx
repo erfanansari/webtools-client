aa6f651c5f65249b10e65536af3fc2d779eaf5c4
Erfan Ansari <dev.erfanansari@gmail.com>
2022-10-12 20:52:35 +0330

    feat: add ReactQueryDevtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={queryClient}>
             <Component {...pageProps} />
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     )
 }
