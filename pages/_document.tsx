16f983eb54ff3241c44a9ef3bfd6274b6d71dc22
Erfan Ansari <dev.erfanansari@gmail.com>
2022-10-16 22:01:35 +0330

    chore: solve MEME error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,14 +9,8 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&&display=swap"
                     rel="stylesheet"
                 /> */}
-                <link
-                    href="https://fonts.googleapis.com/css2?Sora:wght@300;400;500;600;700;800&display=swap"
-                    rel="stylesheet"
-                />
-                <link
-                    href="https://unpkg.com/prismjs@1.20.0/themes/prism-okaidia.css"
-                    rel="stylesheet"
-                />
+                <link href="https://fonts.googleapis.com/css2?Sora:wght@300;400;500;600;700;800&display=swap" />
+                <link href="https://unpkg.com/prismjs@1.20.0/themes/prism-okaidia.css" />
                 {/* <link rel="icon" href="/favicon.ico" /> */}
                 <link rel="icon" href="/black-gear.png" />
             </Head>
