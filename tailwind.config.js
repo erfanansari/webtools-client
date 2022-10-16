a8c76e923f992d520f279afaf4bc750d809c75de
Erfan Ansari <dev.erfanansari@gmail.com>
2022-10-16 17:20:17 +0330

    feat: add color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    main: '#fb310a',
+                    dark: '#981d05',
+                },
+                secondary: {
+                    main: '#03032d',
+                    dark: '#10091d',
+                },
+                neutral: {
+                    main: '#a1a1c0',
+                    light: '#a1a1c0',
+                    lighter:
+                        'linear-gradient(to top, var(--neutralLight) 50%, #fff 120%)',
+                    dark: '#6f6f86',
+                },
+                gray: {
+                    1: '#eef6ff',
+                    2: '#e6f0fe',
+                },
+            },
+        },
     },
     plugins: [],
 }
