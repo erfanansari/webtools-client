530370dc8a3b40d187528b71e170380605cf755c
Erfan Ansari <dev.erfanansari@gmail.com>
2022-10-16 23:48:15 +0330

    feat: add bookmarks page

---
+++ pages/bookmarks.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Layout from '../components/Layout'
+
+const Bookmarks: NextPage = () => {
+    return (
+        <Layout title="Bookmarks">
+            <h1>Bookmark</h1>
+        </Layout>
+    )
+}
+
+export default Bookmarks
