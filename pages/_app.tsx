d3812d0b80c4844da65f1a9077353b6411c6f452
Erfan Ansari <dev.erfanansari@gmail.com>
2022-10-21 14:57:39 +0330

    chore: do not retry failed requests

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,13 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: false,
+        },
+    },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
